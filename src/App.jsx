8cf3a3a89735fc880700b8be5755be99d81755b4
Luis Cortes <luiscortes1986@gmail.com>
2023-08-24 00:02:41 +0200

    sin terminar, tuve que irme y estoy atascado y es muy tarde

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import { useState } from "react";
+import FoodBox from "./components/FoodBox";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+//el eliminar no funciona
+  const handleDeletefood = (id) => {
+       let filteredArr = foods.filter((eachfood) => {
+      if (eachfood.id === id) {
+        return false
+      } else {
+        return true
+      }
+    })
+
+    
+    setFoods(filteredArr)
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      {foods.map((food, i) => (
+      <FoodBox key={i} food={food} onClick={handleDeletefood}/>
+      ))}
+      {/*iteraccion 2
+       <FoodBox
+        food={{
+          name: "Orange",
+          calories: 85,
+          image: "https://i.imgur.com/abKGOcv.jpg",
+          servings: 1
+        }}
+      /> */}
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,25 @@
 // Your code here
+import React from "react";
+
+const FoodBox = ({ food }) => {
+  const { id,name, calories, image, servings } = food;
+  const totalCalories = calories * servings;
+
+  const handleDeleteButton=()=>{
+    onClick(id)
+  }
+  return (
+    <div> 
+      <p>{name}</p>
+      <img src={image}  width={120}/>
+      <p>Calories: {calories}</p>
+      <p>Servings: {servings}</p>
+      <p>
+        <b>Total Calories: {totalCalories} kcal</b>
+      </p>
+      <button onClick={() => {handleDeleteButton(eachfood.id)}}>Delete</button>
+    </div>
+  )
+}
+
+export default FoodBox;
